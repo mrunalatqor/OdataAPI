005ca35d03172c95775878a391c73276d3276e5a
Mrunalsinh Jadeja <mrunalsinh.jadeja@bitscape.com>
2024-02-19 14:05:07 +0530

    test1
    test1

--- Donet7API-OData-v8-Demo-master/Program.cs
+++ Donet7API-OData-v8-Demo-master/Program.cs
@@ -25,11 +25,8 @@ builder.Services.AddDbContext<MyWorldDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors(x => x
                 .AllowAnyMethod()
                 .AllowAnyHeader()
